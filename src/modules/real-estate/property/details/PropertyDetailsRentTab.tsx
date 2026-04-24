49e08c9fc6d35d4a89908407400416e0f6c2fd5e
michaelabelola <michaelabelola@gmail.com>
2026-04-23 22:08:21 -0400

    Enhance DataTable integration for rent definitions and improve API type safety

--- src/modules/real-estate/property/details/PropertyDetailsRentTab.tsx
+++ src/modules/real-estate/property/details/PropertyDetailsRentTab.tsx
@@ -6,6 +6,10 @@ import {RentDefinitionRequest} from "#/modules/real-estate/rent-definition/api.h
 import DataTable from "#/components/data-table/data-table.tsx";
 import type {ColumnDef} from "@tanstack/react-table";
 import {RentDefinitionModel} from "#/modules/real-estate/rent-definition/model.ts";
+import {QuickToolTip} from "#/components/ui/tooltip.tsx";
+import {ButtonGroup} from "#/components/ui/button-group.tsx";
+import {Button, buttonVariants} from "#/components/ui/button.tsx";
+import {Check} from "lucide-react";
 
 const columns: ColumnDef<RentDefinitionModel.RentDefinition>[] = [
     {
@@ -35,6 +39,21 @@ const columns: ColumnDef<RentDefinitionModel.RentDefinition>[] = [
         header: "Default",
         cell: ({row}) => {
             return (`${row.original.duration} ${row.original.durationUnit}`)
+        }
+    },
+    {
+        accessorKey: "action",
+        header: "Action",
+        cell: () => {
+            return (
+                <ButtonGroup>
+                    <QuickToolTip asChild content="Edit Rent Definition">
+                        <Button className={buttonVariants({variant: "outline"})}>
+                            <Check className="w-4 h-4"/>
+                        </Button>
+                    </QuickToolTip>
+                </ButtonGroup>
+            )
         }
     }
 ]
