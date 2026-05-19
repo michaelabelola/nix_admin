1ea379c1f1318c79bb6e3590fb03efd0497452b0
michaelabelola <michaelabelola@gmail.com>
2026-05-19 16:59:05 -0400

    Export useQueryCountries function and set staleTime to Infinity for improved caching

--- src/modules/location/location.hook.ts
+++ src/modules/location/location.hook.ts
@@ -3,12 +3,13 @@ import countriesApi from "#/modules/location/apis/countries.api.ts";
 import {useResponseFieldErrorHandler} from "#/lib/request.types.tsx";
 
 export namespace CountryAPI {
-    function useQueryCountries() {
+    export function useQueryCountries() {
         const errHandler = useResponseFieldErrorHandler()
         return {
             ...useQuery({
                 queryKey: ["location", "countries", "all"],
                 queryFn: () => countriesApi.getAllCountries(),
+                staleTime: Infinity
             }),
             errHandler
         }
